049d99f7b8387a0835dd875456fb8f79bda20ea5
Xiao Lin <linxiao9292@outlook.com>
2016-08-30 22:47:00 +0800

    mvn install before mvn test

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/env sh
 cp -n ~/.m2/settings.xml{,.orig} ; \
 wget -q -O - https://raw.githubusercontent.com/opendaylight/odlparent/master/settings.xml > ~/.m2/settings.xml
+mvn clean install
