cf9bcd4de9e319fe78f61e99da453b151b0e3c21
Xiao Lin <linxiao9292@outlook.com>
2016-08-31 14:44:35 +0800

    Remove a unused command

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,4 +1,3 @@
 #! /usr/bin/env sh
-cp -n ~/.m2/settings.xml{,.orig} ; \
 wget -q -O - https://raw.githubusercontent.com/opendaylight/odlparent/master/settings.xml > ~/.m2/settings.xml
-mvn clean install -DskipTests
+mvn clean install
